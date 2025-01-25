661616d8b21614a1068e1122674a082de8c4f968
Ryan Moakes <m015485n@student.staffs.ac.uk>
2025-01-25 15:47:32 +0000

    Basic Bubble Blower (Broke)

--- GGJ Microwave Studios/Assets/Scripts/Advanced_Player_Movement.cs
+++ GGJ Microwave Studios/Assets/Scripts/Advanced_Player_Movement.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 [BurstCompile]
 public class Advanced_Player_Movement : MonoBehaviour
 {
-
     private Rigidbody2D m_RB;
     private Vector2 m_PlayerDirection;
 
     [Header("Options:")]
-    [Tooltip("Defaults are 1.0f")]
+    [Tooltip("Default is 1.0f")]
     [SerializeField] private float m_MoveSpeed = 1.0f;
+
+    [Tooltip("Default is 1.0f")]
     [SerializeField] private float m_Drag = 1.0f;
 
-    void Awake()
+    private void Awake()
     {
         m_RB = gameObject.GetComponent<Rigidbody2D>();
+
+        if (!m_RB)
+        {
+            Debug.LogError("Rigidbody2D is missing!", this);
+        }
     }
 
     // Floaty Bubble
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         m_RB.linearDamping = m_Drag;
-        m_PlayerDirection.x = m_MoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        m_PlayerDirection.y = m_MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        m_PlayerDirection.x = m_MoveSpeed * Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
+        m_PlayerDirection.y = m_MoveSpeed * Input.GetAxis("Vertical") * Time.fixedDeltaTime;
         m_RB.AddForce(m_PlayerDirection);
     }
-
-
 }
---
+++ GGJ Microwave Studios/Assets/Scripts/BubbleBlower.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class BubbleBlower : MonoBehaviour
+{
+    [Header("Options:")]
+    [Tooltip("This should be the prefab of the attack")]
+    [SerializeField] private GameObject m_BubblePrefab;
+
+    [Tooltip("This should be the transform of the player pointer")]
+    [SerializeField] private Transform m_PointerTransform;
+
+    private Pointer_Aiming m_Aiming;
+
+    private bool m_Attcking = false;
+
+    private void Awake()
+    {
+        m_Aiming = GetComponent<Pointer_Aiming>(); 
+        
+    }
+
+    private void FixedUpdate()
+    {
+      
+
+        if (m_Attcking)
+        {
+            Instantiate(m_BubblePrefab,m_PointerTransform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            m_Attcking = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            m_Attcking = false;
+        }
+    }
+}
